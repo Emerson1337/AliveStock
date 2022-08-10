fc36bc5943ff01d91f2bd5588ab376470a9282e6
Emerson <emersonlucenaoficial@gmail.com>
2022-08-10 00:20:05 -0300

    🚚 Added path mapping to Jest

--- API/jest.config.js
+++ API/jest.config.js
@@ -73,7 +73,12 @@ module.exports = {
   moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "node"],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    "^Controllers/(.*)$": "<rootDir>/src/controllers/$1",
+    "^Services/(.*)$": "<rootDir>/src/services/$1",
+    "^Modules/(.*)$": "<rootDir>/src/modules/$1",
+    "^@src/(.*)$": "<rootDir>/src/$1",
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
