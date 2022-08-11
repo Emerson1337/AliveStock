d194dff8b0055c3fb10e91c29eec5147d78e4701
Emerson <emersonlucenaoficial@gmail.com>
2022-08-10 23:45:27 -0300

    ✅ Created test to get stock history quote

---
+++ API/src/modules/QuotesHistory/__tests__/QuotesHistoryTests.spec.ts
@@ -0,0 +1,22 @@
+import app from "@src/app";
+import request from "supertest";
+
+describe("Quote controller", () => {
+  it("Should be able to get stock quote history", async () => {
+    const response = await request(app).get(
+      `/stocks/IBM/history?from=2022-07-04&to=2022-07-07`
+    );
+    expect(typeof response.body.name).toBe("string");
+    expect(typeof response.body.prices).toBe("object");
+    expect(response.statusCode).toBe(200);
+  });
+
+  it("Should not be able to get stock quote history", async () => {
+    const response = await request(app).get(
+      `/stocks/IBM2/history?from=2022-07-04&to=2022-07-07`
+    );
+
+    expect(response.body.errors).toBe("Stock history quotes not found!");
+    expect(response.statusCode).toBe(404);
+  });
+});
